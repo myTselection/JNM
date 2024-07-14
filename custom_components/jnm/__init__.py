6b883a647e62efb957c179eacb27e73ed90913cd
myTselection <myTselection@gmail.com>
2024-07-14 23:10:16 +0200

    fix await asyn setup

--- custom_components/jnm/__init__.py
+++ custom_components/jnm/__init__.py
@@ -47,11 +47,9 @@ async def async_setup(hass: HomeAssistant, config: dict):
     except ValueError:
         pass
 
-    hass.async_create_task(
-        await hass.config_entries.flow.async_init(
+    await hass.config_entries.flow.async_init(
             DOMAIN, context={"source": config_entries.SOURCE_IMPORT}, data={}
         )
-    )
     return True
 
 async def async_update_options(hass: HomeAssistant, config_entry: ConfigEntry):
@@ -71,9 +69,7 @@ async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up component as config entry."""
-    hass.async_create_task(
-        await hass.config_entries.async_forward_entry_setup(config_entry, Platform.SENSOR)
-    )
+    await hass.config_entries.async_forward_entry_setup(config_entry, Platform.SENSOR)
     # _LOGGER.info(f"{DOMAIN} register_services")
     # register_services(hass, config_entry)
     return True
